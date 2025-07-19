4ca8c24757916b603905726d13ad2be089873fa7
山海 <zijiex365@gmail.com>
2025-07-19 23:18:17 +0800

    feat(bootloader): implement bootloader

---
+++ bootloader/src/main.rs
@@ -0,0 +1,167 @@
+#![no_main]
+#![no_std]
+
+use core::{arch::asm, ptr::write_bytes, slice::from_raw_parts_mut};
+use uefi::{
+    CStr16, Status,
+    boot::{
+        AllocateType, MemoryType, OpenProtocolAttributes, OpenProtocolParams, allocate_pages,
+        exit_boot_services, get_handle_for_protocol, get_image_file_system, image_handle,
+        open_protocol,
+    },
+    entry, guid,
+    proto::{
+        console::gop::{GraphicsOutput, PixelFormat},
+        media::file::{File, FileAttribute, FileMode, FileType},
+    },
+    table::system_table_raw,
+};
+use xmas_elf::{ElfFile, program::Type};
+
+fn load_kernel() -> Result<u64, Status> {
+    const NAME: &str = "kernel";
+    let mut buffer = [0u16; NAME.len() + 1];
+    let name = CStr16::from_str_with_buf(NAME, &mut buffer).map_err(|_e| Status::LOAD_ERROR)?;
+
+    let mut file_system = get_image_file_system(image_handle()).map_err(|e| e.status())?;
+    let mut root = file_system.open_volume().map_err(|e| e.status())?;
+    let handle = root
+        .open(name, FileMode::Read, FileAttribute::empty())
+        .map_err(|e| e.status())?;
+    let mut file = match handle.into_type().map_err(|e| e.status())? {
+        FileType::Regular(f) => f,
+        FileType::Dir(_d) => return Err(Status::LOAD_ERROR),
+    };
+
+    const HEADER_SIZE: usize = 64;
+    const PROGRAM_HEADER_SIZE: usize = 56;
+    const MAX_PROGRAM_HEADER_COUNT: usize = 9;
+    let mut buffer = [0u8; HEADER_SIZE + MAX_PROGRAM_HEADER_COUNT * PROGRAM_HEADER_SIZE];
+    file.set_position(0).map_err(|e| e.status())?;
+    let read_size = file.read(&mut buffer).map_err(|e| e.status())?;
+    let elf = ElfFile::new(&buffer).map_err(|_e| Status::LOAD_ERROR)?;
+
+    // Check
+    let ph_size = elf.header.pt2.ph_entry_size() as usize;
+    let ph_count = elf.header.pt2.ph_count() as usize;
+    if PROGRAM_HEADER_SIZE != ph_size
+        || MAX_PROGRAM_HEADER_COUNT < ph_count
+        || read_size < HEADER_SIZE + ph_count * ph_size
+    {
+        return Err(Status::LOAD_ERROR);
+    }
+
+    for ph in elf.program_iter() {
+        if ph.get_type().map_err(|_e| Status::LOAD_ERROR)? != Type::Load {
+            continue;
+        }
+
+        let offset = ph.offset();
+        let physical_addr = ph.physical_addr();
+        let file_size = ph.file_size() as usize;
+        let mem_size = ph.mem_size() as usize;
+
+        const PAGE_SIZE: usize = 0x1000;
+        let pages = allocate_pages(
+            AllocateType::Address(physical_addr),
+            MemoryType::LOADER_DATA,
+            (mem_size + PAGE_SIZE - 1) / PAGE_SIZE,
+        )
+        .map_err(|e| e.status())?
+        .as_ptr();
+        if pages as u64 != physical_addr {
+            return Err(Status::LOAD_ERROR);
+        }
+
+        let mut buffer = unsafe {
+            write_bytes(pages, 0, mem_size);
+            from_raw_parts_mut(pages, file_size)
+        };
+        file.set_position(offset).map_err(|e| e.status())?;
+        let read_size = file.read(&mut buffer).map_err(|e| e.status())?;
+        if read_size < file_size {
+            return Err(Status::LOAD_ERROR);
+        }
+    }
+    Ok(elf.header.pt2.entry_point())
+}
+
+fn get_graphics_output_config() -> Result<(usize, usize, usize, usize), Status> {
+    let handle = get_handle_for_protocol::<GraphicsOutput>().map_err(|e| e.status())?;
+    let mut protocol = unsafe {
+        open_protocol::<GraphicsOutput>(
+            OpenProtocolParams {
+                handle,
+                agent: image_handle(),
+                controller: None,
+            },
+            OpenProtocolAttributes::GetProtocol,
+        )
+    }
+    .map_err(|e| e.status())?;
+
+    let mode_info = protocol.current_mode_info();
+    if !matches!(
+        mode_info.pixel_format(),
+        PixelFormat::Rgb | PixelFormat::Bgr
+    ) {
+        return Err(Status::LOAD_ERROR);
+    }
+    let (width, height) = mode_info.resolution();
+    let stride = mode_info.stride();
+
+    let mut frame_buffer = protocol.frame_buffer();
+    let frame_buffer_base = frame_buffer.as_mut_ptr() as usize;
+
+    Ok((frame_buffer_base, width, height, stride))
+}
+
+fn get_rsdp_addr() -> Result<u64, Status> {
+    let system_table = unsafe { system_table_raw().ok_or(Status::LOAD_ERROR)?.as_ref() };
+    for i in 0..system_table.number_of_configuration_table_entries {
+        let table = unsafe {
+            system_table
+                .configuration_table
+                .add(i)
+                .as_ref()
+                .ok_or(Status::LOAD_ERROR)?
+        };
+        if table.vendor_guid == guid!("8868e871-e4f1-11d3-bc22-0080c73c8881") {
+            return Ok(table.vendor_table as u64);
+        }
+    }
+    Err(Status::LOAD_ERROR)
+}
+
+#[entry]
+fn main() -> Status {
+    let entry = match load_kernel() {
+        Ok(addr) => addr,
+        Err(e) => return e,
+    };
+
+    let (frame_buffer_base, width, height, stride) = match get_graphics_output_config() {
+        Ok(info) => info,
+        Err(e) => return e,
+    };
+
+    let rsdp_addr = match get_rsdp_addr() {
+        Ok(p) => p,
+        Err(e) => return e,
+    };
+
+    unsafe {
+        let _mmo = exit_boot_services(None);
+
+        asm!(
+            "jmp {}",
+            in(reg) entry,
+            in("r8") frame_buffer_base,
+            in("r9") width,
+            in("r10") height,
+            in("r11") stride,
+            in("r12") rsdp_addr,
+            options(noreturn),
+        );
+    }
+}
