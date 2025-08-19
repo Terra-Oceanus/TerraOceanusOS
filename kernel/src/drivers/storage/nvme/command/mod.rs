fb60026d7a15fcf64d33fe780d4ec489fd8a5653
山海 <zijiex365@gmail.com>
2025-08-19 21:21:19 +0800

    refactor(kernel/drivers/storage/nvme): move Command struct to new location

--- kernel/src/drivers/storage/nvme/command/mod.rs
+++ kernel/src/drivers/storage/nvme/command/mod.rs
@@ -1,3 +1,85 @@
 //! Command
 
 pub mod admin;
+
+#[repr(C, packed)]
+pub struct Submission {
+    /// Command Dword 0
+    /// - Bits 0 ..= 7: OPC for Opcode
+    ///   - Bits 0 ..= 1: DTD for Data Transfer Direction
+    ///     - 0b00: No Data Transfer
+    ///     - 0b01: Host to Controller Transfer
+    ///     - 0b10: Controller to Host Transfer
+    ///     - 0b11: Bi-Directional Transfers
+    ///   - Bits 2 ..= 7: FN for Function
+    /// - Bits 8 ..= 9: FUSE for Fused Operation
+    ///   - 0b00: Normal operation
+    ///   - 0b01: First command of Fused operation
+    ///   - 0b10: Second command of Fused operation
+    ///   - 0b11: Reserved
+    /// - Bits 10 ..= 13: Reserved
+    /// - Bits 14 ..= 15: PSDT for PRP or SGL for Data Transfer
+    ///   - 0b00: PRPS Used
+    ///   - 0b01: SGLs Used MPTR Address
+    ///   - 0b10: SGLs Used MPTR SGL Segment
+    ///   - 0b11: Reserved
+    /// - Bits 16 ..= 31: CID for Command Identifier
+    cdw0: u32,
+
+    /// Namespace Identifier
+    /// - 0x0: Not used
+    /// - 0xFFFFFFFF: Broadcast
+    nsid: u32,
+
+    /// Command Dword 2
+    cdw2: u32,
+
+    /// Command Dword 3
+    cdw3: u32,
+
+    /// Metadata Pointer
+    mptr: u64,
+
+    /// Data Pointer
+    /// - PRP if CDW0.PSDT is 0b00
+    ///   - Bits 0 ..= 7: PRP1 for PRP Entry 1
+    ///   - Bits 8 ..= 15: PRP2 for PRP Entry 2
+    /// - SGL1 for SGL Entry 1 if CDW0.PSDT is 0b01 ..= 0b10
+    pub dptr: u128,
+
+    /// Command Dword 10
+    cdw10: u32,
+
+    /// Command Dword 11
+    cdw11: u32,
+
+    /// Command Dword 12
+    cdw12: u32,
+
+    /// Command Dword 13
+    cdw13: u32,
+
+    /// Command Dword 14
+    cdw14: u32,
+
+    /// Command Dword 15
+    cdw15: u32,
+}
+impl Submission {
+    pub fn null() -> Self {
+        Self {
+            cdw0: 0,
+            nsid: 0,
+            cdw2: 0,
+            cdw3: 0,
+            mptr: 0,
+            dptr: 0,
+            cdw10: 0,
+            cdw11: 0,
+            cdw12: 0,
+            cdw13: 0,
+            cdw14: 0,
+            cdw15: 0,
+        }
+    }
+}
