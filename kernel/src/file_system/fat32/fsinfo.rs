f777f416e3d4c566650d3c0dc5103a3e616bf711
山海 <zijiex365@gmail.com>
2025-09-07 13:51:21 +0800

    fix(kernel/fs/fat32): correct expected value in signature check

--- kernel/src/file_system/fat32/fsinfo.rs
+++ kernel/src/file_system/fat32/fsinfo.rs
@@ -29,9 +29,9 @@ impl Memory for FSI {}
 impl FSI {
     pub fn validate(&self) -> bool {
         self.lead_sig == 0x41615252
-            && self.reserved0 == [0; 480]
-            && self.struc_sig == 0x51417272
-            && self.reserved1 == [0; 12]
+            && self.struc_sig == 0x61417272
             && self.trail_sig == 0xAA550000
+            && self.reserved0 == [0; 480]
+            && self.reserved1 == [0; 12]
     }
 }
